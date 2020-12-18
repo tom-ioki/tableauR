4522fd6d25021ebc93cf87e04edfb0102f669e7a
Tobias Matz <tobias.matz@ioki.com>
2020-12-18 18:02:06 +0100

    Move check_for_api_error to validations and add tests for utils

--- R/utils.R
+++ R/utils.R
@@ -9,26 +9,3 @@ compose_server_url <- function(url, version, end_point) {
 
   return(url)
 }
-
-check_for_api_error <- function(api_response) {
-  response <- content(api_response)
-
-  if (is.null(response$error)) {
-    response <-
-      list(
-        status = "success",
-        response = response
-      )
-    return(response)
-  }
-
-  error <- response$error
-  response <-
-    list(
-      status = "error",
-      error_code = error$code,
-      error_summary = error$summary,
-      error_detail = error$detail
-    )
-  return(response)
-}
